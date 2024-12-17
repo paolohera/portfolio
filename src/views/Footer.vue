1e5aa3cfc6f4c17bf047efe539bc95042a76756d
paolohera <143242348+paolohera@users.noreply.github.com>
2024-12-17 13:48:50 +0800

    Update Footer.vue

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -9,7 +9,7 @@ const address = 'Osmeña St. Gun-Ob Lapu-Lapu City, Cebu. Philippines';
     
       <!-- Social Links -->
       <div class="footer-section">
-        <h4>Contact Me</h4>
+        <h4>Get in touch</h4>
         <div class="social-icons">
           <a href="https://www.facebook.com/paopao.hera" target="_blank" class="social-icon">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
